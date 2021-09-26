af67dd1bea014192e278762cd2d730fa1868fb5e
songyang <songyang719@163.com>
2021-09-25 21:37:39 -0400

    add assignment 2

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSort.java
@@ -6,7 +6,12 @@ package edu.neu.coe.info6205.sort.elementary;
 import edu.neu.coe.info6205.sort.BaseHelper;
 import edu.neu.coe.info6205.sort.Helper;
 import edu.neu.coe.info6205.sort.SortWithHelper;
+import edu.neu.coe.info6205.util.Benchmark;
+import edu.neu.coe.info6205.util.Benchmark_Timer;
 import edu.neu.coe.info6205.util.Config;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
@@ -57,8 +62,16 @@ public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
      */
     public void sort(X[] xs, int from, int to) {
         final Helper<X> helper = getHelper();
-
         // TO BE IMPLEMENTED
+        for (int i = from + 1; i < to; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (helper.compare(xs, j, j + 1) > 0) {
+                    helper.swap(xs, j, j + 1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     public static final String DESCRIPTION = "Insertion sort";
@@ -66,4 +79,68 @@ public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
     public static <T extends Comparable<T>> void sort(T[] ts) {
         new InsertionSort<T>().mutatingSort(ts);
     }
+
+    public static void main(String[] args) throws IOException {
+        int nRuns = 1000;
+        for (int i = 200; i <= 3200; i *= 2) {
+            // random array
+            int n = i;
+            BaseHelper<Integer> helper = new BaseHelper<>("InsertionSort", n,
+                    Config.load(InsertionSort.class));
+            SortWithHelper<Integer> sorter = new InsertionSort<>(helper);
+            Benchmark<Integer[]> bm1 = new Benchmark_Timer<>(
+                    "random input insertion sort run time", sorter::preProcess,
+                    sorter::sort);
+            double x1 = bm1.run(helper.random(Integer.class, r -> r.nextInt(n)), nRuns);
+            System.out.println("when n is " + n + ", run time is " + x1);
+
+            // ordered array
+            Benchmark<Integer[]> bm2 = new Benchmark_Timer<>(
+                    "ordered input insertion sort run time", sorter::preProcess,
+                    sorter::sort);
+            double x2 = bm2.run(orderedArray(n), nRuns);
+            System.out.println("when n is " + n + ", run time is " + x2);
+
+
+            // partially ordered array
+            Benchmark<Integer[]> bm3 = new Benchmark_Timer<>(
+                    "partially ordered input insertion sort run time",
+                    sorter::preProcess,
+                    sorter::sort);
+            double x3 = bm3.run(partiallyOrderedNumber(n, helper), nRuns);
+            System.out.println("when n is " + n + ", run time is " + x3);
+
+
+            // reverse ordered array
+            Benchmark<Integer[]> bm4 = new Benchmark_Timer<>(
+                    "reverse ordered input insertion sort run time", sorter::preProcess,
+                    sorter::sort);
+            double x4 = bm4.run(reversOrderedNumber(n), nRuns);
+            System.out.println("when n is " + n + ", run time is " + x4);
+
+        }
+    }
+
+    private static Integer[] orderedArray(int n) {
+        Integer[] xs = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            xs[i] = i;
+        }
+        return xs;
+    }
+
+    private static Integer[] partiallyOrderedNumber(int n, BaseHelper<Integer> helper) {
+        Integer[] xs = helper.random(Integer.class, r -> r.nextInt(n));
+        Arrays.sort(xs, xs.length / 2, xs.length);
+        return xs;
+    }
+
+    private static Integer[] reversOrderedNumber(int n) {
+        Integer[] xs = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            xs[i] = n - i - 1;
+        }
+        return xs;
+    }
+
 }
--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.util;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -55,7 +56,23 @@ public class Timer {
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
         // TO BE IMPLEMENTED: note that the timer is running when this method is called and should still be running when it returns.
-        return 0;
+        pause();
+        for (int i = 0; i < n; i++) {
+            T t = supplier.get();
+            // deal with npe test result
+            if (preFunction != null) {
+                t = preFunction.apply(t);
+            }
+            // time function
+            resume();
+            U u = function.apply(t);
+            pauseAndLap();
+            // deal with npe test result
+            if (postFunction != null) {
+                postFunction.accept(u);
+            }
+        }
+        return meanLapTime();
     }
 
     /**
@@ -174,7 +191,7 @@ public class Timer {
      */
     private static long getClock() {
         // TO BE IMPLEMENTED
-        return 0;
+        return System.nanoTime();
     }
 
     /**
@@ -186,7 +203,7 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // TO BE IMPLEMENTED
-        return 0;
+        return TimeUnit.NANOSECONDS.toMillis(ticks);
     }
 
     final static LazyLogger logger = new LazyLogger(Timer.class);
